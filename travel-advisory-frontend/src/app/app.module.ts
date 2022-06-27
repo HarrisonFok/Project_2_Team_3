ee0bcecdb7e3ab495380a15b192d34d670a25832
HarrisonFok <harrison.fok@gmail.com>
2022-06-27 15:51:04 -0400

    Adding a service to fetch the request

--- travel-advisory-frontend/src/app/app.module.ts
+++ travel-advisory-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
